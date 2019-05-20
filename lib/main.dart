4c78ec616256f4ad47b2d3f55f5d7907d3d1d81c
Lincxx <bossco28@gmail.com>
2019-05-19 21:54:56 -0400

    Added to buttons and Column layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Quizzler());
+  runApp(Quizzet());
 }
 
-class Quizzler extends StatelessWidget {
+class Quizzet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,6 +17,57 @@ class Quizzler extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                "QUESTIONS HERE",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: FlatButton(
+              color: Colors.green,
+              textColor: Colors.white,
+              child: Text("True"),
+              onPressed: () {},
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: FlatButton(
+              color: Colors.red,
+              onPressed: () {},
+              textColor: Colors.white,
+              child: Text("False"),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
